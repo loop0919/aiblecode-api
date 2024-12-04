db82f89e50b3a6bbb2ebffdff4d18e19bfe0ed25
loop0919 <ruupu2001@gmail.com>
2024-12-04 07:37:45 +0000

    🦀 refreshではなくflush でした

--- api/core/security.py
+++ api/core/security.py
@@ -63,7 +63,7 @@ def create_session(db: Session, token: str) -> str:
     session = user_model.Session(id=session_id, token=f"Bearer {token}")
     db.add(session)
     db.commit()
-    db.refresh(session)
+    db.flush()
     return session_id
 
 
@@ -72,6 +72,7 @@ def delete_session(db: Session, session_id: str):
     if session:
         db.delete(session)
         db.commit()
+        db.flush()
 
 
 # HeaderまたはCookieからjwtトークンを認証
--- api/routers/user.py
+++ api/routers/user.py
@@ -40,6 +40,34 @@ def user_list(
     return user_crud.get_user_list(db)
 
 
+@router.get(
+    "/user/{username}",
+    tags=["user"],
+    response_model=user_schema.Message,
+)
+def user_detail(
+    username: str, db=Depends(database.get_db), user=Depends(get_current_active_user)
+) -> user_schema.Message:
+    """
+    ユーザーの詳細情報を取得する。
+    """
+    got_user = user_crud.get_user_by_username(db, username)
+
+    if user == got_user:
+        return user_schema.Message(
+            status="success",
+            message="User found, and it's you",
+            user=user_schema.User(id=user.id, username=user.username),
+        )
+    else:
+
+        return user_schema.Message(
+            status="success",
+            message="User found, but it's not you",
+            user=user_schema.User(id=user.id, username=user.username),
+        )
+
+
 # 新規のユーザー情報(username, password)を受信後、ユーザーを作成
 @router.post(
     "/signup",
